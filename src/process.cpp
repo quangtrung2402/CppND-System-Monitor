707837e097ed66c11562db418d116dfe55aff434
Quang Trung <quangtrung2402@gmail.com>
2023-07-04 22:31:22 +0700

    Add solution

--- src/process.cpp
+++ src/process.cpp
@@ -5,29 +5,41 @@
 #include <vector>
 
 #include "process.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
-// TODO: Return this process's ID
-int Process::Pid() { return 0; }
+Process::Process(int pid) :
+m_pid(pid),
+m_utilization(0),
+m_uptime(LinuxParser::UpTime(pid)),
+m_ram(LinuxParser::Ram(pid)),
+m_user(LinuxParser::User(pid)),
+m_command(LinuxParser::Command(pid))
+  {
+  long seconds{LinuxParser::UpTime() - m_uptime};
+  long time{LinuxParser::ActiveJiffies(pid)};
+  try {
+    m_utilization = float(time) / float(seconds);
+  } catch (...) {
+    m_utilization = 0;
+  }
+}
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+int Process::Pid() const { return m_pid; }
 
-// TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+float Process::CpuUtilization() const { return m_utilization; }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Command() const { return m_command; }
 
-// TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::Ram() const { return m_ram; }
 
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+string Process::User() const { return m_user; }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+long int Process::UpTime() const { return m_uptime; }
+
+bool Process::operator<(Process const& a) const {
+  return m_utilization < a.m_utilization;
+}
